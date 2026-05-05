8c0f4a5aa76edc1b805fb17999f9199b8f6518c2
Lee Clayton <leehart32@gmail.com>
2026-05-05 14:46:51 -0600

    BREAK: Removing S3 public access block to test AC-3 enforcement

--- lab-3-4/main.tf
+++ lab-3-4/main.tf
@@ -55,13 +55,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "vault" {
   }
 }
 
-resource "aws_s3_bucket_public_access_block" "vault" {
-  bucket                  = aws_s3_bucket.vault.id
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-}
+
 
 # Refuse bucket deletion from anyone except the account root.
 data "aws_caller_identity" "current" {}
