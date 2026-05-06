66b4c9ed2f8bb305d2482a28240e12ca6fd8fd71
Lee Clayton <leehart32@gmail.com>
2026-05-06 15:29:43 -0600

    Fix: Declare project_name variable for GRC gate compatibility

--- baselines/gcp/variables.tf
+++ baselines/gcp/variables.tf
@@ -3,3 +3,9 @@ variable "gcp_project" {
   description = "The GCP Project ID where resources will be deployed"
   default     = "grc-lab54-1778095737"
 }
+
+variable "project_name" {
+  type        = string
+  description = "Project name passed from the CI/CD pipeline"
+  default     = "grc-engineering-capstone"
+}
